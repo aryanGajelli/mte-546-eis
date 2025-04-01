33f5b0a5fa50348c48dcb5a7096b26a771177ee6
Philip Zhu <philipchu888@gmail.com>
2025-04-01 00:32:07 -0400

    generate freq script

--- iterate_for_offsets.py
+++ iterate_for_offsets.py
@@ -1,9 +1,9 @@
 import numpy as np
-f = np.array([0.1, 0.13, 0.16, 0.21, 0.27])
-t = np.arange(0, 100, 0.01)
+f = np.array([0.1, 0.13, 0.16, 0.21, 0.27, 0.34, 0.43, 0.55, 0.71, 0.9])
+t = np.arange(0, 50, 0.01)
 best_M = float('inf')
-for _ in range(10000000):
-    phi = np.random.uniform(0, 2*np.pi, 5)
+for _ in range(500000):
+    phi = np.random.uniform(0, 2*np.pi, 10)
     s = np.sum(np.sin(2 * np.pi * f[:, None] * t + phi[:, None]), axis=0)
     M = np.max(np.abs(s))
     if M < best_M:
