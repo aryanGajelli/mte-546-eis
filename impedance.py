f64559643eabd7659ad6c128604e5d02f6d1c7f5
aryanGajelli <argajelli@gmail.com>
2025-04-03 20:59:09 -0400

    get impedances

--- impedance.py
+++ impedance.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from read_bin import load_data
 from data_cleaner import break_data, break_hf
 from scipy import optimize
@@ -19,31 +20,44 @@ def fit_sine_wave(t, y, f=1):
     # Fit the sine wave to the data
     params, _ = optimize.curve_fit(sin_func, xdata=t, ydata=y, p0=initial_guess, method='trf', maxfev=100000,
                                    bounds=([-np.inf, -2*np.pi, -np.inf], [np.inf, 2 * np.pi, np.inf]))
-    print(f"{params[0]:.6f}sin({f:.3f}*2*pi*t + {params[1]:.6f}) + {params[2]:.6f}")
+    # print(f"{params[0]:.6f}sin({f:.3f}*2*pi*t + {params[1]:.6f}) + {params[2]:.6f}")
     return sin_func(t, *params), params[0], params[1]
 
+def get_impedances(cell, temp, soc):
+    df, fs = load_data(cell, temp, soc)
+    lf_v, hf_v = break_data(df, volt=True)
+    lf_i, hf_i = break_data(df, volt=False)
 
-df, fs = load_data(cell=79, temp='35C', soc=50)
-lf_v, hf_v = break_data(df, volt=True)
-lf_i, hf_i = break_data(df, volt=False)
+    f1_10v, f10_100v, f100_1000v = break_hf(hf_v)
+    fv = f1_10v + f10_100v + f100_1000v
 
-f1_10v, f10_100v, f100_1000v = break_hf(hf_v)
+    f1_10i, f10_100i, f100_1000i = break_hf(hf_i)
+    fi = f1_10i + f10_100i + f100_1000i
+    impedances = []
+    for i in range(len(fv)):
+        f = FREQUENCY_SWEEP_F1_1000[i]
+        tv = fv[i].index
+        ti = fi[i].index
+        _, *v_params = fit_sine_wave(tv, fv[i], f=f)
+        _, *i_params = fit_sine_wave(ti, fi[i], f=f)
 
-f1_10i, f10_100i, f100_1000i = break_hf(hf_i)
+        v_freq = v_params[0] * np.exp(1j * v_params[1])
+        i_freq = i_params[0] * np.exp(1j * i_params[1])
 
-tv = f1_10v[0].index
-ti = f1_10i[0].index
-v, *v_params = fit_sine_wave(tv, f1_10v[0], f=1)
-i, *i_params = fit_sine_wave(ti, f1_10i[0], f=1)
+        impedance = (v_freq / -i_freq)
+        impedance = impedance.conjugate()
+        impedances.append(impedance)
+    
+    return pd.Series(impedances, index=FREQUENCY_SWEEP_F1_1000, name=f"Impedance {cell} {temp} {soc}% SOC")
 
-v_freq = v_params[0] * np.exp(1j * v_params[1])
-i_freq = i_params[0] * np.exp(1j * i_params[1])
+z = get_impedances(79, '45C', 10)
+plt.plot(z.values.real, z.values.imag, label='Cell 79 45C 100% SOC')
+plt.xlabel('Real Part (Ohm)')
+plt.ylabel('Imaginary Part (Ohm)')
+plt.title('Nyquist Plot')
+plt.grid()
+plt.legend()
+plt.show()
 
 
-# for i, (v_param, i_param) in enumerate(zip(v_params, i_params)):
-#     v_freq = v_param * np.exp(1j * (PHI[i] + v_offset))
-#     i_freq = i_param * np.exp(1j * (PHI[i]))
-impedance = (v_freq / -i_freq)
-impedance = impedance.conjugate()
-print(f"Impedance at {FREQUENCY_SWEEP_F1_1000[0]} Hz: {impedance:.6f}")
 
